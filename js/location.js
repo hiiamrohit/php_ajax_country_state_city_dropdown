a09ea1680342296758dcc126bb5d36cf234c0b7b
Atharva Deshmukh <arthar360@users.noreply.github.com>
2016-10-04 22:00:30 +0530

    Update location.js

--- js/location.js
+++ js/location.js
@@ -8,8 +8,6 @@
 
           var errorRes = function(e) {
               console.log(e);
-              //alert("Error found \nError Code: "+e.status+" \nError Message: "+e.statusText);
-              //$('#loader').modal('hide');
           }
             $.ajax({
                 url: url,
